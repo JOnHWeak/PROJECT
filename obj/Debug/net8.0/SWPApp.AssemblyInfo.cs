ed2479c3bf4c1e5a0399c831f60c6b4a81adee7c
JOnHWeak <tranminhtridbk39@gmail.com>
2024-07-01 22:11:11 +0700

    passwoerd

--- obj/Debug/net8.0/SWPApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SWPApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWPApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7115abf1b67ac68116c079249378d01d03da13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+434b92ad9cba9e50e84606becf46deefe469397c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWPApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWPApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
